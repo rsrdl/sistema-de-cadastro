59e8a7d5ba57046c83413726b539d6543a393c72
rsrdl <olivanlima@gmail.com>
2023-06-12 21:43:28 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
 // mais um console logde teste
 console.log("O Paulo fez esse commit sem você ver na aula!");
+console.log("Enviando para o GitHub");
 
