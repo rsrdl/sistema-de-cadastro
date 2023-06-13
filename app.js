1a1d936521be46d14ec8ef17cd8863633f423340
rsrdl <olivanlima@gmail.com>
2023-06-12 21:34:03 -0300

    Console log indicando modificações do Paulo

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
+// mais um console logde teste
+console.log("O Paulo fez esse commit sem você ver na aula!");
+
